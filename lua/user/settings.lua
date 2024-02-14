64a7ae233af0eb30648f8c8a27fc9957cc9cfc70
jczhang02 <jczhang@live.it>
2024-02-15 00:03:56 +0800

    feat: change

--- lua/user/settings.lua
+++ lua/user/settings.lua
@@ -2,7 +2,7 @@
 local settings = {}
 
 -- Examples
-settings["use_ssh"] = true
+settings["use_ssh"] = false
 settings["use_copilot"] = false
 settings["colorscheme"] = "catppuccin-latte"
 settings["background"] = "light"
